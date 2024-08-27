f86f7b1ad5dbd096b42563ab531fe180cc18c28f
Davi Taveira <devtaveira@gmail.com>
2024-08-27 20:23:45 -0300

    teste

--- frota-leve-web/src/app/page.tsx
+++ frota-leve-web/src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>hello</h1>
+      <h1 className="bg-red-500">hello</h1>
       <ButtonDavi text={"LOGIN"} />
     </main>
   );
