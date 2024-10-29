c533eab8a55c1e3431aaf1d2b14e58bf3e05f1cd
Milena <mkamitami@gmail.com>
2024-10-29 19:30:41 -0300

    alteracao na cor da sidebar

--- frota-leve-web/src/components/Sidebar.tsx
+++ frota-leve-web/src/components/Sidebar.tsx
@@ -4,12 +4,12 @@ import { FaPeopleGroup } from 'react-icons/fa6'
 
 const Sidebar = () => {
   return (
-    <div className='w-64 bg-gray-200 h-full px-6 py-3 text-black fixed top-0 left-0'>
+    <div className='w-64 bg-[#0b0f38] h-full px-6 py-3 text-whitte fixed top-0 left-0'>
       <div className='my-2 mb-4'>
-        <h1 className='text-2x text-black font-bold'> Admin Dashboard</h1>
+        <h1 className='text-2x text-white font-bold'> Admin Dashboard</h1>
       </div>
       <hr className='border-t-2 border-black' />
-      <ul className='mt-3 text-black font-bold'>
+      <ul className='mt-3 text-white font-bold'>
         <li className='mb-2 rounded hover:shandow hover:bg-[#FFC314] py-2 pl-2'>
           <Link href='/' className='psx-3 flex items-center'>
             <FaHome className='inline-block w6 h-6 mr-2 -mt-2'></FaHome>
